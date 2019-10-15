34648dc16dece3f1e633727be836a47c654a0998
Panikiran <pvpkiran@gmail.com>
2019-10-15 10:06:36 +0200

    Initial Commit

---
+++ src/main/java/in/phani/git/mavenversioning/MavenversioningApplication.java
@@ -0,0 +1,13 @@
+package in.phani.git.mavenversioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenversioningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenversioningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/phani/git/mavenversioning/MavenversioningApplicationTests.java
@@ -0,0 +1,16 @@
+package in.phani.git.mavenversioning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MavenversioningApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
